91884dcb37c60c2c847b2d310ea9a2f747d1241a
Konrad Malik <konrad.malik@gmail.com>
2019-11-10 17:49:39 +0100

    try not to filter as there is some strange error

--- scripts/almond-install.sh
+++ scripts/almond-install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # need to override hadoop to support http!
-# we will filter out 'curator' jars as they are then reimported with new hadoop
+# we will filter out 'curator' jars as they are then reimported with new hadoop 
+# .filter(_.contains(\"curator\"))
 # also disable logs for 'org' to not pollute all notebook
 ./almond --install --global \
 --predef-code " 
   val jars = java.nio.file.Files.list(java.nio.file.Paths.get(\"${SPARK_HOME}/jars\")).toArray.map(_.toString)
-    .filter(_.contains(\"curator\"))
     .map { fname =>
         val path = java.nio.file.FileSystems.getDefault().getPath(fname)
         ammonite.ops.Path(path)
     }
   interp.load.cp(jars)
-  import \$ivy.\`org.apache.hadoop:hadoop-client:${HADOOP_VERSION}\`
   import \$ivy.\`sh.almond::almond-spark:${ALMOND_VERSION}\`
+  import \$ivy.\`org.apache.hadoop:hadoop-client:${HADOOP_VERSION}\`
   import org.apache.log4j.{Level, Logger}
   Logger.getLogger(\"org\").setLevel(Level.OFF)
   " 
